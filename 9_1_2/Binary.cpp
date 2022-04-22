2c9a8eaf408dc270de4a694ee3060cc0343eff4c
Nikita <nikita2003.ns@gmail.com>
2022-04-22 16:52:11 +0300

    Logical fix

--- 9_1_2/Binary.cpp
+++ 9_1_2/Binary.cpp
@@ -1,6 +1,6 @@
 #include "Binary.h"
 #include <iostream>
-
+#include <cstdint>
 Binary::Binary(uint8_t value, uint8_t pattern)
 {
 	this->value = value;
@@ -9,12 +9,12 @@ Binary::Binary(uint8_t value, uint8_t pattern)
 
 Binary Binary::operator&=(Binary& other)
 {
-	return Binary(value = value & (other.value & ~(pattern & other.pattern)), pattern);
+	return Binary(value = ((value) & ~(pattern & other.pattern)), pattern);
 }
 
 Binary Binary::operator|=(Binary& other)
 {
-	return Binary(value = value | (other.value & (pattern & other.pattern)), pattern);
+	return Binary(value = (value) | (other.value & (pattern & other.pattern)), pattern);
 }
 
 void Binary::Output()
